0920e8fe56eb843bc529019a8bbaffca101b696d
Julio Rincon <julio.rincon@transport.nsw.gov.au>
2015-08-13 16:59:54 +1000

    magit don't ask when pushing...

--- personal/personal.el
+++ personal/personal.el
@@ -4,3 +4,8 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; ==== magit defaults ============
+(setq magit-push-always-verify nil)
+
+;; personal.el ends here
