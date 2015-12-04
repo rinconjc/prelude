06d6816198610fb4f18d290ec5f4a81d0e780e1e
Julio <jcrinconc@gmail.com>
2015-12-04 21:14:33 +1100

    refactor clojure

--- personal/personal.el
+++ personal/personal.el
@@ -8,4 +8,18 @@
 ;; ==== magit defaults ============
 (setq magit-push-always-verify nil)
 
+(package-refresh-contents)
+(unless (package-installed-p 'clj-refactor)
+  (package-install 'clj-refactor))
+
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import statements
+  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
 ;; personal.el ends here
