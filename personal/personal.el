8359c7ef731b61e99b17af6ce5f727a99131336f
jrincon2 <julio.rincon@macquarie.com>
2019-09-03 16:15:57 +1000

    minor bindings

--- personal/personal.el
+++ personal/personal.el
@@ -6,6 +6,8 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+(global-set-key (kbd "C-s-c") 'sp-copy-sexp)
+(ffap-bindings)
 ;; =========== custom overrides ============
 
 (setq prelude-guru nil)
